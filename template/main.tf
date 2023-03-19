47f11911de8922447719aec81f143521dd674b02
Elferey <elf.yanov@gmail.com>
2023-03-19 17:14:28 +0300

    add tf file

--- template/main.tf
+++ template/main.tf
@@ -1,7 +1,7 @@
 resource "virtualbox_vm" "node" {
   count     = 2
   name      = format("node-%02d", count.index + 1)
-  image     = "D:\\Soft\\Образы\\ubuntu-20.04.4-live-server-amd64.iso"
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20230317.0.0/providers/virtualbox.box"
   cpus      = 2
   memory    = "512 mib"
   user_data = "ubuntu:${file("~/.ssh/yandex_key_ssh.pub")}"
