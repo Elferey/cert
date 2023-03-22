19a3a1cd8318d6ffdcf5dcb08f90a4f8de3a74d5
Elferey <elf.yanov@gmail.com>
2023-03-22 19:41:13 +0300

    static ip

--- template/main.tf
+++ template/main.tf
@@ -47,3 +47,31 @@ resource "yandex_compute_instance" "vm-4" {
      ssh-keys = "ubuntu:${file("~/.ssh/yandex_key_ssh.pub")}"
   }
 }
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_vm_1" {
+  value = "192.168.10.100"
+}
+
+output "external_ip_address_vm_2" {
+  value = "192.168.10.101"
+}
