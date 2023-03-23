2780f9e9241a7048b85b81066aaa6214137a5b63
Elferey <elf.yanov@gmail.com>
2023-03-23 15:46:12 +0300

    add

--- ansible/roles/awscli.sh
+++ ansible/roles/awscli.sh
@@ -2,7 +2,7 @@
 
 aws=`aws --version | awk '{print $1}'`
 
-if [[ $? == 1 ]]; then
+if [[ $aws != "aws-cli/2.11.5" ]]; then
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     ./aws/install
